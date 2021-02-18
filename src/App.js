ac02112ac64569adaa8694e0f81de91f0bdcf470
korf44 <yurietraci@gmail.com>
2021-02-18 17:49:10 +0200

    Some setup for deeploy

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import './AppStyle.css';
+import api from './api/api';
+import dateBuilder  from './data';
 
 function App() {
+
+  const [query, setQuery] = useState('');
+  const [weather, setWeather] = useState({})
+
+  const search = evt => {
+    if (evt.key === "Enter") {
+      fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
+        .then(res => res.json())
+        .then(result => {
+          setWeather(result);
+          setQuery('');
+          console.log(result);
+        });
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={(typeof weather.main != "undefined") ? ((weather.main.temp > 15) ? 'app warm' :
+    'app') : 'app'}>
+    <main>
+        <div className="search-box">
+            <input
+                type="text"
+                className="search-bar"
+                placeholder="Search..."
+                onChange={e => setQuery(e.target.value)}
+                value={query}
+                onKeyPress={search}
+            />
+        </div>
+        {(typeof weather.main != "undefined") ? (
+            <div>
+                <div className="location-box">
+                    <div className="location">{weather.name}, {weather.sys.country}</div>
+                    <div className="date">{dateBuilder(new Date())}</div>
+                </div>
+                <div className="weather-box">
+                    <div className="temp">
+                        {Math.round(weather.main.temp)}°С
+          </div>
+                    <div className="weather">{weather.weather[0].main}</div>
+                    <div className="description">{weather.weather[0].description}</div>
+                </div>
+            </div>
+        ) : ('')}
+    </main>
     </div>
-  );
+)
 }
 
 export default App;
---
+++ src/api/api.js
@@ -0,0 +1,7 @@
+
+const api = {
+    key: "2eef4aac97288b64083083d45c2b3e8e",
+    base: "https://api.openweathermap.org/data/2.5/"
+  }
+
+  export default api
